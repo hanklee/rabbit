511df01d90aa652f3161604233c892cd9b1e23ea
Xianling Li <hank.dev@gmail.com>
2018-01-10 02:48:16 +0800

    support the json array

---
+++ src/main/java/com/lixianling/rabbit/conf/DBObjectConfig.java
@@ -0,0 +1,11 @@
+/**
+ * Create time: 08-Jan-2018
+ */
+package com.lixianling.rabbit.conf;
+
+/**
+ *
+ * @author hank
+ */
+public class DBObjectConfig {
+}
---
+++ src/test/java/com/lixianling/rabbit/redis/Gencontent.java
@@ -0,0 +1,11 @@
+/**
+ * Create time: 09-Jan-2018
+ */
+package com.lixianling.rabbit.redis;
+
+/**
+ *
+ * @author hank
+ */
+public class Getcontent {
+}
