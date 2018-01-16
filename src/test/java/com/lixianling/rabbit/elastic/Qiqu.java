fe8539a90469793f0b871c24d09e63954800926c
Xianling Li <hank.dev@gmail.com>
2018-01-17 00:43:30 +0800

    update new code

---
+++ src/test/java/com/lixianling/rabbit/elastic/Qiqu.java
@@ -0,0 +1,11 @@
+/**
+ * Create time: 15-Jan-2018
+ */
+package com.lixianling.rabbit.elastic;
+
+/**
+ *
+ * @author hank
+ */
+public class Qiqu {
+}
---
+++ src/test/java/com/lixianling/rabbit/redis/Category.java
@@ -0,0 +1,11 @@
+/**
+ * Create time: 15-Jan-2018
+ */
+package com.lixianling.rabbit.redis;
+
+/**
+ *
+ * @author hank
+ */
+public class Category {
+}
